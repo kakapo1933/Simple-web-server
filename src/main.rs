ab8baeaef97b6f2b9e2d2cd9d55d1e8db68f2194
Kasper Chen <neko12377@gmail.com>
2022-08-21 00:24:30 +0800

    add basic response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use::std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -20,8 +21,16 @@ fn handle_connection(mut stream: TcpStream) {
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
+        // The browser signals the end of an HTTP request by sending two newline characters in a row,
+        // so to get one request from the stream, we takes lines until we get a line that is empty string.
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
