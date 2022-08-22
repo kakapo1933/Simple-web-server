fdf6543ec8fd8a4cf8b76e4b150b2063c647cb5e
Kasper Chen <neko12377@gmail.com>
2022-08-23 01:34:58 +0800

    handle request info and route separately

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use::std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -26,16 +28,39 @@ fn handle_connection(mut stream: TcpStream) {
     //     .take_while(|line| !line.is_empty())
     //     .collect();
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (request_method, request_path) = handle_request_information(buf_reader);
 
-    let (status_line, files) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", fs::read_to_string("webPages/hello.html"))
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("webPages/404.html"))
-    };
-    let contents = files.unwrap();
+    let (status_line, file_name) = handle_route(&request_method, &request_path);
+
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn handle_request_information(buf_reader: BufReader<&mut TcpStream>) -> (String, String) {
+    let buf_reader: BufReader<&mut TcpStream> = buf_reader.into();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_method = request_line.split(" ").collect::<Vec<&str>>()[0].to_string();
+    let request_path = request_line.split(" ").collect::<Vec<&str>>()[1].to_string();
+    (request_method, request_path)
+}
+
+fn handle_route <'a>(request_method: &'a str, request_path: &'a str) -> (&'a str, &'a str) {
+    let (status_line, file_name) = match request_method {
+        "GET" => {
+            match request_path {
+                "/" => ("HTTP/1.1 200 OK", "webPages/hello.html"),
+                "/sleep" => {
+                    thread::sleep(Duration::from_secs(5));
+                    ("HTTP/1.1 200 OK", "webPages/hello.html")
+                },
+                _ => ("HTTP/1.1 404 NOT FOUND", "webPages/404.html"),
+            }
+        },
+        "POST" => ("HTTP/1.1 404 NOT FOUND", "webPages/404.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "webPages/404.html"),
+    };
+    (status_line, file_name)
 }
